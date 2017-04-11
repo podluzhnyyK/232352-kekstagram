5784fdfc7b43ee721d79e8b0ea00eaa0277b11fb
podluzhyyK <podluzhnyy94@mail.ru>
2017-04-11 08:10:42 +0400

    #9 Личный проект: милый DOM проходит тест

--- js/pictures.js
+++ js/pictures.js
@@ -17,7 +17,7 @@ overlayUpload.classList.add('invisible');
 
 var getRandom = function (min, max) {
   return Math.round(Math.random() * (max - min) + min);
-}
+};
 
 var comments = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце-концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как-будто их избивают. Как можно было поймать такой неудачный момент?!'];
 
@@ -47,7 +47,7 @@ var showImage = function (data) {
   document.querySelector('.gallery-overlay-image').src = data.url;
   document.querySelector('.likes-count').textContent = data.likes;
   document.querySelector('.comments-count').textContent = data.comments;
-}
+};
 
 showImage(pictures[4]);
 
